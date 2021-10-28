166527ccf71107499a1a0b2a9785439af3f54f56
12Bren <brandonj1914@gmail.com>
2021-10-28 07:02:28 -0600

    shared file update day 2 initial equation finished

--- SharedPracticeFile.py
+++ SharedPracticeFile.py
@@ -1,3 +1,4 @@
+import math
 # day one
 print("Hello world \n" * 3)
 print("Hello " + "Bren")
@@ -47,10 +48,33 @@ print("b: " + "b")
 # print("Your band name could be " + cityName + " " + petName)  # read: Your band name could be, concatenates cityName,
 # concatenates " ", concatenates petName, Your band name could be cityName petName, executes print, compilation finished
 # END OF DAY 1:^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# DAY 2:
-
-
-
+# DAY 2: data types
+# test code final
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill? $"))
+numberOfPeople = float(input("How many people to split the bill? "))
+tipAmount = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+offNum = 12, 10, 15
+if tipAmount == 10:
+    billAmount = bill / numberOfPeople
+    tipDivided = tipAmount*billAmount / 100
+    inTotal = round(billAmount + tipDivided, 2)
+    print("Each person should pay: " + "$" + str(inTotal))
+if tipAmount == 12:
+    billAmount = bill / numberOfPeople
+    tipDivided = tipAmount * billAmount / 100
+    inTotal = round(billAmount + tipDivided, 2)
+    print("Each person should pay: " + "$" + str(inTotal))
+if tipAmount == 15:
+    billAmount = bill / numberOfPeople
+    tipDivided = tipAmount * billAmount / 100
+    inTotal = round(billAmount + tipDivided, 2)
+    print("Each person should pay: " + "$" + str(inTotal))
+if tipAmount != offNum:
+    billAmount = bill / numberOfPeople
+    tipDivided = tipAmount * billAmount / 100
+    inTotal = round(billAmount + tipDivided, 2)
+    print("Each person should pay: " + "$" + str(inTotal))
 
 
 
--- Thonny debuging system studt.py
+++ Thonny debuging system studt.py
@@ -1,2 +1,9 @@
-name = input("what is your name? ")
-print("Hello " + name), print("there are " + str(len(name)) + " characters in your name!")
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill? $"))
+numberOfPeople = float(input("How many people to split the bill? "))
+tipAmount = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+if tipAmount == 10:
+    billAmount = bill / numberOfPeople
+    tipDivided = 100 / tipAmount
+    inTotal = billAmount + tipDivided
+    print("Each person should pay: " + "$" + str(inTotal))
